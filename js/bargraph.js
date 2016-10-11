de2573bdc89a8e9835ffd95623aa2c34e41e0b0a
Michelle <michelchan421@gmail.com>
2016-10-11 17:03:33 -0400

    Made a new branch to try out another way of making the bars.

--- js/bargraph.js
+++ js/bargraph.js
@@ -32,62 +32,127 @@ function makeGraph(){
 	var arrayMin = Math.floor(d3.min(array));
 	var arrayMax = Math.floor(d3.max(array));
 
-	var scale = d3.scale.linear()
-					.domain([arrayMin,arrayMax])
-					.range([0,100]);
+	// var x = d3.scaleLinear()
+	// 		.domain([0, countsValue.length])
+	// 		.range([0,width]);
+	var x = d3.scale.linear()
+		.domain([0, countsValue.length])
+		.range([0,width]);
+
+	// var y = d3.scaleLinear()
+	// 		.domain([arrayMin,arrayMax]);
+	var y = d3.scale.linear()
+		.domain([arrayMin, arrayMax])
 
 	for (var i = 0; i<array.length; i++){
-		adjusted[i] = scale(array[i]);
+		adjusted[i] = x(array[i]);
 	}
 
-	var svg = d3.select("#barGraph")
-		.append("svg")
+	var barGraph = d3.select("#barGraph")
+		.append("svg:svg")
 		.attr("width", width)
 		.attr("height", height);
 
-	svg.selectAll("text")
-		.data(countsValue)
-		.enter()
-		.append("text")
-		.text(function(d) {
-			return d;
-		})
-		.attr("x", function(d, i) {
-			return i * (width / numAttr)-100;
-		})
-		.attr("y", function(d){
-			return height - d;
-		})
-		.attr("font-family", "sans-serif")
-		.attr("font-size", (width/numAttr)/4 + "px")
-		.attr("fill", "purple")
-		.attr("text-anchor", "middle");
-
-	svg.selectAll("rect")
+	barGraph.selectAll("rect")
 		.data(adjusted)
 		.enter()
-		.append("rect")
+		.append("svg:rect")
 		.attr("x", function(d,i){
-			return i * (width/numAttr);
+			return x(i)
 		})
 		.attr("y", function(d){
-			return height - d;
+			return height - y(d);
+		})
+		.attr("height", function(d) {
+			return y(d);
 		})
 		.attr("width", width/numAttr - padding)
-		.attr("height", function(d){
-			return d;
-		})
-		.attr("fill", function(d){
-			return "rgb("+ d +", 233 , 30)";
-		})
+		.attr("fill","green")
+
+	// barGraph.selectAll("rect")
+	// 	.data(countsValue)
+	// 	.enter()
+	// 	.append("svg.title")
+	// 	.text(function(d){
+	// 		return d;
+	// 	});
+
+	barGraph.selectAll("rect")
 		.on("mouseover", function(d){
-			var hoveredBar = d3.select(this).style({opacity:'0.8'});
-			hoveredBar.select("text").style({opacity:'1.0'});
+			d3.select(this)
+				.style({opacity:'0.8'});
 		})
 		.on("mouseout", function(d){
 			var outBar = d3.select(this).style({opacity:'1.0'});
-			outBar.select("text").style({opacity:'0'});
 		});
+
+	// barGraph.selectAll("text")
+	// 	.append("svg:text")
+
+	// barGraph.selectAll("text.yAxis")
+	// 	.data(countsValue)
+	// 	.enter()
+	// 	.append("svg:text")
+	// 	.attr("x", function(d,i){
+	// 		return x(i)+(width/numAttr - padding)
+	// 	})
+	// 	.attr("y", height)
+	// 	.attr("dx", -(width/numAttr - padding)/2)
+	// 	.attr("text-anchor", "middle")
+	// 	.attr("style", "font-family: Helvetica, sans-serif")
+	// 	.text(function(d){
+	// 		return d;
+	// 	})
+	// 	.attr("class","yAxis");
+
+	// var svg = d3.select("#barGraph")
+	// 	.append("svg")
+	// 	.attr("width", width)
+	// 	.attr("height", height);
+
+	// svg.selectAll("rect")
+	// 	.data(adjusted)
+	// 	.enter()
+	// 	.append("rect")
+	// 	.attr("x", function(d,i){
+	// 		return i * (width/numAttr);
+	// 	})
+	// 	.attr("y", function(d){
+	// 		return height - d;
+	// 	})
+	// 	.attr("width", width/numAttr - padding)
+	// 	.attr("height", function(d){
+	// 		return d;
+	// 	})
+	// 	.attr("fill", function(d){
+	// 		return "rgb("+ d +", 233 , 30)";
+	// 	})
+	// 	.on("mouseover", function(d){
+	// 		var hoveredBar = d3.select(this).style({opacity:'0.8'});
+	// 		hoveredBar.select("text").style({opacity:'1.0'});
+	// 	})
+	// 	.on("mouseout", function(d){
+	// 		var outBar = d3.select(this).style({opacity:'1.0'});
+	// 		outBar.select("text").style({opacity:'0'});
+	// 	});
+
+	// svg.selectAll("text")
+	// 	.data(countsValue)
+	// 	.enter()
+	// 	.append("text")
+	// 	.text(function(d) {
+	// 		return d;
+	// 	})
+	// 	.attr("x", function(d, i) {
+	// 		return i * (width / numAttr)-100;
+	// 	})
+	// 	.attr("y", function(d){
+	// 		return height - d;
+	// 	})
+	// 	.attr("font-family", "sans-serif")
+	// 	.attr("font-size", (width/numAttr)/4 + "px")
+	// 	.attr("fill", "purple")
+	// 	.attr("text-anchor", "middle");
 }
 
 function putValuesInArray(){
@@ -113,7 +178,6 @@ function loadSelect(){
 	for (i=1; i<attr.length+1; i++){
 		selectHTML += "<option value="+ i +">" + attributes[i] +"</option>\n";
 	}
-	console.log(selectHTML);
 	$('#bargraphSelect').append(selectHTML);
 
 	$('#bargraphSelect').trigger('contentChanged');
@@ -121,7 +185,6 @@ function loadSelect(){
 }
 
 $(document).ready(function() {
-	initBarGraph(1);
 	$('select').not('.disabled').material_select();
 	$('#bargraphSelect').on('contentChanged', function() {
 	  	// re-initialize (update)
