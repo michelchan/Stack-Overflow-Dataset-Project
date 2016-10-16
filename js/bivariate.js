e071ec8f8b41711284dfbd836d48fe9ad118936a
Michelle <michelchan421@gmail.com>
2016-10-15 23:34:23 -0400

    More cleanup and fixed something in bargraph.

--- js/bivariate.js
+++ js/bivariate.js
@@ -1,5 +1,5 @@
 var data;
-var counts = {};
+var rows = {};
 var width = $(window).width()* .7;
 var height = $(window).height() * .8;
 var padding = 5;
@@ -11,9 +11,29 @@ d3.json("./data/datawithInts.json", function(error, json){
 	$("#loading").hide();
 	loadSelect();
 	data = json;
-	initBarGraph();
+	initBivariateGraph();
 });
 
 function initBivariateGraph () {
+	makeBivariateGraph();
+}
+
+function makeBivariateGraph() {
+	var numAttr = Object.keys(rows).length;
+	var countsValue = putValuesInArray();
+	var adjusted = [];
+
+	var arrayMin = Math.ceil(d3.min(array));
+	var arrayMax = Math.ceil(d3.max(array));
+
+	var x = d3.scale.linear()
+		.domain([0, countsValue.length])
+		.range([0,width]);
+
+	var y = d3.scale.linear()
+		.domain([arrayMin, arrayMax]);
+
 	var xAxis = d3.svg.axis();
+	xAxis.scale(x);
+	xAxis.orient("bottom");
 }
